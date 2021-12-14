85789351126cf02a2eee8381e44aa10e00919d8b
azmi romzi <azmiromzihoesaini@gmail.com>
2021-12-14 19:27:36 +0700

    menambahkan tag p

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>coba php</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam voluptatibus molestiae error ipsum aliquid impedit ullam. Maxime nemo consequatur iure pariatur accusantium cum soluta nobis in dolorum quo, suscipit corporis.</p>
+</body>
+</html>
